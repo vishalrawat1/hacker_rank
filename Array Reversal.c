9ae600c7501d4a82055a39f4a45fadcbd09779fa
vishalrawat1 <91478776+vishalrawat1@users.noreply.github.com>
2022-02-17 21:00:19 +0530

    Create Array Reversal.c

---
+++ Array Reversal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d ", &arr[i]);
+    }
+    for(int j=n-1; j>=0 ; j--){
+        printf("%d ",arr[j]);
+    }
+
+}
